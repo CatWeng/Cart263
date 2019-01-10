1f474742c9376a15b91cdd6005e994dc0533f46c
Catherine <31903385+CatWeng@users.noreply.github.com>
2019-01-10 17:21:34 -0500

    Add files via upload

---
+++ Assignment 1/template-p5-project/js/script.js
@@ -0,0 +1,87 @@
+"use strict";
+
+/*****************
+
+Title of Project
+Author Name
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+
+******************/
+
+// preload()
+//
+// Description of preload
+let avatar = {
+  x:0,
+  y:0,
+  maxSize: 64,
+  size: 64,
+  active: true,
+  color: '#cccc55'
+}
+
+let food = {
+  x:100,
+  y:100,
+  size: 64,
+  color: '#FFFFFF'
+}
+
+function preload() {
+
+}
+
+function setup() {
+createCanvas(windowWidth, windowHeight);
+food.x = random(windowWidth-food.size/2);
+food.y = random(windowHeight-food.size/2);
+//noCursor();
+}
+
+function draw() {
+  if (!avatar.active) {
+    return;
+  }
+  background(50, 90, 200);
+  displayFood();
+  displayAvatar();
+  updateAvatar();
+  collision();
+}
+
+function displayAvatar() {
+  push();
+  noStroke();
+  fill(avatar.color);
+  ellipse(avatar.x,avatar.y,avatar.size);
+  pop();
+}
+
+function updateAvatar() {
+  avatar.x = mouseX,
+  avatar.y = mouseY,
+  avatar.size = constrain(avatar.size, 0, avatar.maxSize);
+  avatar.size -= 0.5;
+  if (avatar.size == 0){
+    avatar.active = false;
+  }
+}
+
+function displayFood() {
+  push();
+  noStroke();
+  fill(food.color);
+  ellipse(food.x,food.y,food.size);
+  pop();
+}
+
+function collision() {
+  let d = dist(avatar.x,avatar.y,food.x,food.y);
+  if (d < avatar.size/2 + food.size/2) {
+    avatar.size = constrain(avatar.size + 50,0,avatar.maxSize);
+    food.x = random(0,width);
+    food.y = random(0,height);
+  }
+}
